beb0e004661fdc3f322934962c10c1429ae27c80
DARK OSINT <levianawulandari7@gmail.com>
2025-10-16 15:58:45 +0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+# Utility functions for the application.
+
+import time
+
+def get_current_timestamp():
+    """Get the current timestamp as a string."""
+    return time.strftime("%Y-%m-%d %H:%M:%S")
